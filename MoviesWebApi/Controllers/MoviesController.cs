6e75fc3fd3160eb9fe75af565375d3ac55e929b3
Ivan Gamanovich <therealigmiller@gmail.com>
2023-08-24 21:39:06 +0300

    init: proj

---
+++ MoviesWebApi/Controllers/MoviesController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MoviesWebApi.Models;
+
+namespace MoviesWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MoviesController : ControllerBase
+    {
+        private readonly MovieContext _dbContext;
+
+        public MoviesController(MovieContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        // GET api/Movies
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Movie>>> GetMovies()
+        {
+            if(_dbContext.Movies == null)
+            {
+                return NotFound();
+            }
+
+            return await _dbContext.Movies.ToListAsync();
+        }
+
+        //Get api/Movies/id
+    }
+}
